fc0c4981e74ce6990154dff3402001e2226c259c
Mark Kropf <markkropf@gmail.com>
2018-04-26 21:37:20 -0400

    migrate concourse target to ssl

--- update-pipeline.sh
+++ update-pipeline.sh
@@ -1,2 +1,2 @@
-fly --target nebulous login --team-name markkropf --concourse-url http://ci.nebulous.company
+fly --target nebulous login --team-name markkropf --concourse-url https://ci.nebulous.company
 fly -t nebulous set-pipeline --pipeline cf-local-docker --config pipeline.yml --load-vars-from ../markkropf/creds/gcp.yml
